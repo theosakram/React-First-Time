d2ffdf021254971134413716ff4fa59447e4c110
theosakram <wyrdhn@gmail.com>
2020-08-06 09:50:12 +0700

    Rocket debouncing

--- sakra-app/src/components/SearchBar.jsx
+++ sakra-app/src/components/SearchBar.jsx
@@ -1,25 +1,50 @@
-import React from "react";
-import { useState } from "react";
-import useFetch from "../hooks/useFetch";
-// import useFetch from "../hooks/useFetch";
+import React, { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import YugiCard from "../components/YugiCard";
+import useDebounce from "../hooks/useDebounce";
 
 function SearchBar() {
-  // const {data, loading, error} = useFetch()
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+
+  const debouncedSearch = useDebounce(searchTerm, 250);
+
+  useEffect(() => {
+    if (debouncedSearch) {
+      setIsSearching(true);
+      fetch(
+        `https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${debouncedSearch}&num=50&offset=0`
+      )
+        .then((results) => {
+          return results.json();
+        })
+        .then(({ data }) => {
+          setIsSearching(false);
+          setResults(data);
+        });
+    } else setResults([]);
+  }, [debouncedSearch]);
+
   return (
-    <div className="container">
-      <div
-        className="field is-small"
-        style={{ marginTop: "25px", width: "300px" }}
-      >
-        <div className="control">
-          <input
-            className="input is-info"
-            type="text"
-            placeholder="Search By Name"
-            // value={fname}
-            // onChange={changeFname}
-            // onSubmit={getCards}
-          ></input>
+    <div style={{ marginTop: "25px" }}>
+      <input
+        placeholder="Search Card by Name"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {isSearching && <Loader />}
+
+      <div className="container" style={{ marginTop: "25px" }}>
+        <div className="columns is-multiline">
+          {results.map((result) => (
+            <YugiCard
+              id={result.id}
+              key={result.id}
+              card={result.name}
+              image={result.card_images[0].image_url}
+            />
+          ))}
         </div>
       </div>
     </div>
